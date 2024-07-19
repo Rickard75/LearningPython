0c954d2bf1eeb8734e05e6fdd80f778bdb1c0be2
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-07-19 19:02:22 +0200

    Added even numbers storing

--- luhn algorithm/luhn.py
+++ luhn algorithm/luhn.py
@@ -10,12 +10,23 @@
 
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
-    card_number_reversed = card_number[-1:-5:-1] # extracts every digit from last to last-4th position    print(card_number_reversed)
-
+    card_number_reversed = card_number[::-1] #reverse the card number
+    odd_digits = card_number_reversed[::2]
+    print("Reversed:         ", card_number_reversed)    
+    print("Reversed and odd: ", odd_digits)  
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+        print("Sum of odd digits: ", sum_of_odd_digits)
+    
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for digit in even_digits:
+        print(digit)
+    
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
-    print(translated_card_number)
+    print("Number extracted: ", translated_card_number)
     verify_card_number(translated_card_number)
 main()
