710176c3714cd118a5a672f91ef743095fc1188a
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-07-19 11:25:16 +0200

    Added default direction

--- vigenere.py
+++ vigenere.py
@@ -1,31 +1,33 @@
+'''Encryption and decryption using the Vigenere cipher'''
+
 text = 'Hello Zaira'
 custom_key = 'python'
 
-def vigenere(message, key, direction):
+def vigenere(message, key, direction=1): # default direction is straight(=encryption)
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
     
         # Append space to the message
         if char == ' ':
-            encrypted_text += char
+            final_message += char
         else:        
-            # Find the right key character to encode
+            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
 
-            # Define the offset and the encrypted letter
+            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset*direction) % len(alphabet)
-            encrypted_text += alphabet[new_index]
+            final_message += alphabet[new_index]
     
-    return encrypted_text
+    return final_message
     
-encryption = vigenere(text, custom_key, 1)
+encryption = vigenere(text, custom_key) # default direction=1: encryption
 print('Plain text:      ', text)
 print('Encrytpted text: ', encryption)
-decryption = vigenere(encryption, custom_key, -1)
+decryption = vigenere(encryption, custom_key, -1) # decryption
 print('Decrypted text:  ', decryption)
