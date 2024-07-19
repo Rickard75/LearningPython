fa4afe2e41347641cc78ac82b545193fb0a1dc02
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-07-19 10:21:22 +0200

    7th commit: now with function

--- freecodecamp.py
+++ freecodecamp.py
@@ -1,13 +1,20 @@
 '''Encrpit text using Caesar Cipher'''
 
-text = 'Hello World'
+text = 'Hello Zaira'
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
-for char in text.lower(): # convert text to lowercase
-    if char == ' ':
-        print('space!')
-    index = alphabet.find(char) # find the index of the character in the alphabet
-    new_index = index + shift # shift the index by the shift value
-    encrypted_text += alphabet[new_index] # add the new character to the encrypted text
-    print('char:', char, 'encrypted text:', encrypted_text) # print the character and the encrypted text
+
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+
+caesar(text,shift)
