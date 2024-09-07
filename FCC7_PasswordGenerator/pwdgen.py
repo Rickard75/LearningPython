bb8a66aaad046b7130c6afd36495fdca9abc08e0
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-09-07 22:59:26 +0200

    Last commit: added comments about regular expressions

--- FCC7_PasswordGenerator/pwdgen.py
+++ FCC7_PasswordGenerator/pwdgen.py
@@ -1,13 +1,40 @@
-import secrets # Secure random number generator (more powerful than random)
-import string  # String constants
+import re
+import secrets
+import string
 
 
-# Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
 
-# Combine all characters
-all_characters = letters + digits + symbols
-print(all_characters)
-print(secrets.choice(all_characters))
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters) # Randomly select a character from all_characters
+        
+        constraints = [                         # Constraints for the password: r stands for raw string (includes backslashes)
+            (nums, r'\d'),                      # At least 1 digit r'\d' is equivalent to r'[0-9]'
+            (special_chars, fr'[{symbols}]'),   # At least 1 special character fr'[{symbols}]' is equivalent to fr'[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]'
+            (uppercase, r'[A-Z]'),              # At least 1 uppercase letter
+            (lowercase, r'[a-z]')               # At least 1 lowercase letter
+        ]
+
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))    # Check if the number of characters in the password meets the constraint
+            for constraint, pattern in constraints
+        ):
+            break
+    
+    return password
+    
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
