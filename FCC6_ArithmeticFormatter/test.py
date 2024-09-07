9d73a6fa5292505a4d64d47b5453e8084d238de2
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-09-07 14:42:28 +0200

    Bug correction: test.py now gets problems in single row

---
+++ FCC6_ArithmeticFormatter/test.py
@@ -0,0 +1,35 @@
+def arithmetic_arranger(problems, show_answers=True):
+    """problems is a list of strings"""
+    first_row = []
+    second_row = []
+    separator_row = []
+    result_row = []
+
+    for pb in problems:
+        addend_list = pb.split(' ')         # separo numeri e segno
+        addend_1 = addend_list[0]           
+        sign     = addend_list[1]
+        addend_2 = addend_list[2]
+
+        row_length = max(len(addend_list[0]),len(addend_list[2])) + 2       
+        first_row.append(' '*(row_length-len(addend_list[0])) + addend_list[0])        #prima riga
+        second_row.append(sign + ' '*(row_length-len(sign)-len(addend_2)) + addend_2)  #seconda riga
+        separator_row.append('-'*row_length)                                           #riga separatrice
+
+        if show_answers:          # controllo per visualizzazione risultato
+            if sign == '+':
+                result = int(addend_1) + int(addend_2)
+            elif sign == '-':
+                result = int(addend_1) - int(addend_2)
+            result_row.append(' '*(row_length-len(str(result))) + str(result))
+
+    # Unire gli elementi di ciascuna lista con quattro spazi e stampare le righe risultanti
+    print('    '.join(first_row))
+    print('    '.join(second_row))
+    print('    '.join(separator_row))
+    if show_answers:
+        print('    '.join(result_row))
+
+    return problems
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
