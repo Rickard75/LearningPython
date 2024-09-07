71ea31851099ca662f9746a5cc6518fde39ceeff
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-09-07 23:05:08 +0200

    Last commit: added long comment about regex

--- FCC7_PasswordGenerator/pwdgen.py
+++ FCC7_PasswordGenerator/pwdgen.py
@@ -1,3 +1,36 @@
+"""Regular expression is a powerful tool for matching patterns in text."""
+
+# Le regex (o espressioni regolari) sono un modo potente per cercare, confrontare e manipolare stringhe di testo in base a schemi specifici.
+# Sono particolarmente utili quando vuoi trovare modelli specifici come numeri, lettere, parole, o formati di testo (es. email, numeri di telefono, ecc.).
+
+# Cosa fanno le regex?
+# Immagina di voler cercare delle cose specifiche in una stringa:
+
+# Tutte le cifre in un testo.
+# Parole che iniziano con una lettera maiuscola.
+# Un indirizzo email in un documento.
+# Le regex ti permettono di creare uno "schema" che rappresenta ciò che stai cercando e poi di usarlo per trovare corrispondenze.
+
+# Alcune regex comuni:
+# \d: Trova un numero.
+# \w: Trova una lettera.
+# \s: Trova uno spazio bianco.
+# . : Trova qualsiasi carattere.
+# ^ : Trova qualcosa all'inizio della stringa.
+# $ : Trova qualcosa alla fine della stringa.
+# [a-z]: Trova qualsiasi lettera minuscola.
+# [A-Z]: Trova qualsiasi lettera maiuscola.
+# [0-9]: Trova qualsiasi numero.
+# [^a]: Trova qualsiasi carattere che non sia 'a'.
+# (a|b): Trova 'a' o 'b'.
+# \b: Trova un bordo di parola.
+# \d{3}: Trova esattamente 3 cifre.
+# \d{3,7}: Trova da 3 a 7 cifre.
+# \d{3,}: Trova 3 o più cifre.
+# \d{,7}: Trova fino a 7 cifre.
+
+
+
 import re
 import secrets
 import string
